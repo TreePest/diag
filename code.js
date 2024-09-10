94f0a8352b6a7775f9b80233ae50bb0c8a5866ae
Luranalberic <hugoharbers@gmail.com>
2024-09-10 22:14:57 +0200

    Update code.js

--- code.js
+++ code.js
@@ -562,11 +562,11 @@ document.addEventListener('DOMContentLoaded', function() {
 	});
 
     // Ajoute des écouteurs pour les changements sur les inputs de filtre
-    document.getElementById('nom').addEventListener('input', function() {
+    document.getElementById('nomInput').addEventListener('input', function() {
     applyFilters();  // Réappliquer les filtres dès qu'il y a une saisie
 	});
 
-    document.getElementById('taille').addEventListener('input', function() {
+    document.getElementById('tailleInput').addEventListener('input', function() {
     applyFilters();  // Réappliquer les filtres dès qu'il y a une saisie
 	});
 
