185e5e665c3f36d1db5cec7c54aa3943c9ff65fe
TreePest <180539157+TreePest@users.noreply.github.com>
2026-06-21 17:30:52 +0200

    Update details.js

--- details.js
+++ details.js
@@ -133,10 +133,10 @@ descriptionContent += `
             <p><strong>La base de données est en cours de développement, les données de chaque fiche doivent encore être vérifiées et sourcées.</strong></p>
             <p><strong>Statut actuel de cette fiche :</strong> ${row[43]}</p>
         </div>
-        <div class="reset-btn">
-            <button onclick="window.open('${formURL}', '_blank')">
-              Signaler une erreur sur cette fiche
-            </button>
-        </div>
     `;
+  <div class="reset-btn">
+      <button onclick="window.open('${formURL}', '_blank')">
+      Signaler une erreur sur cette fiche
+      </button>
+  </div>
 } 
